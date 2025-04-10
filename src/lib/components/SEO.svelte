1bb2799ba7011979a9fb352a3ad6ba99fe734d00
vinney cavallo <vcavallo@gmail.com>
2025-04-10 18:15:58 -0400

    Fix social share 1

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   export let title = 'Catallax';
-  export let description = 'The Catallax network is an open protocol that enables pseudonymous contractor economies for services';
+  export let description = 'Catallax is an open protocol enabling pseudonymous contractor economies';
   export let url = 'https://catallax.network';
-  export let image = '/catallax-logo-1-transparent.png';
+  export let wideImage = '/catallax-social-share-1200-630.png';
+  export let narrowerImage = '/catallax-social-share-800-418.png';
 </script>
 
 <svelte:head>
@@ -15,12 +16,12 @@
   <meta property="og:url" content={url} />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
-  <meta property="og:image" content={image} />
+  <meta property="og:image" content={wideImage} />
   
   <!-- Twitter -->
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:url" content={url} />
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content={image} />
+  <meta name="twitter:image" content={narrowerImage} />
 </svelte:head>
